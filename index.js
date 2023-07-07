6a43ef491acb63991c437f53f37a49a24392e739
Sirvance <126606183+Sirvance@users.noreply.github.com>
2023-07-07 00:57:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const users = [
 ];
 
 function main() {
+    const root =
+        document.getElementById("root");
+    const name =
+        document.createElement("h1");
+    name.innerHTML = "FREELANCERS";
+    root.appendChild(name):
 
 }
 
